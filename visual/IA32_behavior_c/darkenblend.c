1c148da443d6a3166f2ac98da91873defef3c598
uyjulian <uyjulian@gmail.com>
2021-01-11 07:29:25 -0600

    clean old code

--- visual/IA32_behavior_c/darkenblend.c
+++ visual/IA32_behavior_c/darkenblend.c
@@ -7,24 +7,10 @@
 //                                         //
 /////////////////////////////////////////////
 
-#ifdef _WIN32
-#ifdef __GNUC__
-#pragma GCC push_options
-#pragma GCC target("mmx")
-#define __MMX__
-#endif
-#ifdef __clang__
-#pragma clang attribute push (__attribute__((target("mmx"))), apply_to=function)
-#define __MMX__
-#endif
-#endif
-
-#include <mmintrin.h>
 #include "tvpgl_ia32_intf_behavior_c.h"
 
 TVP_GL_IA32_FUNC_DECL(void, TVPDarkenBlend_HDA_c, (tjs_uint32 *dest, const tjs_uint32 *src, tjs_int len))
 {
-#if 1
 	FOREACH_CHANNEL(
 		{
 			if (j % 4 == 3)
@@ -43,17 +29,4 @@ TVP_GL_IA32_FUNC_DECL(void, TVPDarkenBlend_HDA_c, (tjs_uint32 *dest, const tjs_u
 			m &= 0xff;
 			d[j] = m;
 		}, len, src, dest);
-#endif
-#if 0
-	for (tjs_int i = 0; i < len; i += 1)
-	{
-		__m64 m = _mm_cvtsi32_si64(src[i]);
-		__m64 k = _mm_cvtsi32_si64(dest[i] | 0xFFFFFFu);
-		m = _m_psubusb(k, m);
-		m = _m_pand(m, _mm_set1_pi32(0xFFFFFFu));
-		m = _m_psubb(k, m);
-		dest[i] = _mm_cvtsi64_si32(m);
-	}
-	_m_empty();
-#endif
 }

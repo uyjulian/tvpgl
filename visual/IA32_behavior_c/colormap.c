cb58d82b649a99fc259042cc3f18df63b248dd19
uyjulian <uyjulian@gmail.com>
2021-01-11 07:08:22 -0600

    correct colormap/univtrans scalar implementation

--- visual/IA32_behavior_c/colormap.c
+++ visual/IA32_behavior_c/colormap.c
@@ -45,74 +45,22 @@ TVP_GL_IA32_FUNC_DECL(void, TVPApplyColorMap65_c, (tjs_uint32 *dest, const tjs_u
 
 TVP_GL_IA32_FUNC_DECL(void, TVPApplyColorMap65_d_c, (tjs_uint32 *dest, const tjs_uint8 *src, tjs_int len, tjs_uint32 color))
 {
-#if 0
-	// XXX: this doesn't match the MMX version
+#if 1
 	FOREACH_CHANNEL_COLOR(
 		{
-			tjs_uint16 l = src[i];
-			l <<= 8;
-			l += *da;
+			tjs_uint32 l = (dest[i] >> 24) + (src[i] << 8);
 			if (j % 4 == 3)
 			{
 				d[j] = TVPNegativeMulTable65[l];
 				continue;
 			}
+			tjs_uint16 m = d[j];
+			m <<= 8u;
 			tjs_uint16 k = c[j];
 			k -= d[j];
 			k *= TVPOpacityOnOpacityTable65[l];
-			tjs_uint16 m = d[j];
-			m <<= 8u;
 			k += m;
 			k >>= 8u;
-			k += d[j];
-			d[j] = k;
-		}, len, color, dest);
-#else
-	__m64            v7;  // mm7
-	tjs_int32        v8;  // eax
-	__m64            v9;  // mm1
-	__m64            v11; // mm4
-
-	v7 = _m_punpcklbw(_mm_cvtsi32_si64(color & 0xFFFFFF), _mm_setzero_si64());
-	for (tjs_int i = 0; i < len; i += 1)
-	{
-		v8  = (dest[i] >> 24) + (src[i] << 8);
-		__m64 k = _mm_cvtsi32_si64(dest[i]);
-		k = _m_psllqi(k, 0x28u);
-		k = _m_psrlqi(k, 0x28u);
-		v9  = _m_punpcklbw(k, _mm_setzero_si64());
-		v11 = _mm_set1_pi16(TVPOpacityOnOpacityTable65[v8]);
-		__m64 l = _m_psllwi(v9, 8u);
-		__m64 m = _m_psubw(v7, v9);
-		m = _m_pmullw(m, v11);
-		m = _m_paddw(l, m);
-		m = _m_psrlwi(m, 8u);
-		m = _m_packuswb(m, _mm_setzero_si64());
-		dest[i] = (TVPNegativeMulTable65[v8] << 24) | _mm_cvtsi64_si32(m);
-	}
-	_m_empty();
-#endif
-}
-
-TVP_GL_IA32_FUNC_DECL(void, TVPApplyColorMap65_a_c, (tjs_uint32 *dest, const tjs_uint8 *src, tjs_int len, tjs_uint32 color))
-{
-#if 0
-	// XXX: this doesn't match the MMX version
-	FOREACH_CHANNEL_COLOR(
-		{
-			tjs_uint16 k = c[j];
-			if (j % 4 == 3)
-			{
-				k = 1;
-			}
-			k *= src[i];
-			k >>= 6u;
-			tjs_uint16 l = src[i];
-			l *= d[j];
-			l >>= 6u;
-			tjs_uint16 m = d[j];
-			m -= l;
-			k += m;
 			if (k & (1 << 15))
 			{
 				k = 0;
@@ -124,21 +72,67 @@ TVP_GL_IA32_FUNC_DECL(void, TVPApplyColorMap65_a_c, (tjs_uint32 *dest, const tjs
 			d[j] = k;
 		}, len, color, dest);
 #else
-	__m64            v7;  // mm7
-	__m64            v9;  // mm3
-	__m64            v11; // mm1
-
-	v7 = _m_por(_m_punpcklbw(_mm_cvtsi32_si64(color & 0xFFFFFF), _mm_setzero_si64()), _m_psllqi(_mm_cvtsi32_si64(0x100u), 0x30u));
+	__m64 color_unpacked = _m_punpcklbw(_mm_cvtsi32_si64(color & 0xFFFFFF), _mm_setzero_si64());
 	for (tjs_int i = 0; i < len; i += 1)
 	{
-		v9  = _mm_set1_pi16(src[i]);
-		v11 = _m_punpcklbw(_mm_cvtsi32_si64(dest[i]), _mm_setzero_si64());
-		__m64 k = _m_pmullw(v11, v9);
-		k = _m_psrlwi(k, 6u);
-		k = _m_psubw(v11, k);
-		__m64 l = _m_pmullw(v9, v7);
-		l = _m_psrlwi(l, 6u);
-		l = _m_paddw(k, l);
+		tjs_uint32 l = (dest[i] >> 24) + (src[i] << 8);
+		__m64 dest_unpacked = _m_punpcklbw(_mm_cvtsi32_si64(dest[i] & 0xFFFFFF), _mm_setzero_si64());
+		__m64 m = dest_unpacked; 
+		m = _m_psllwi(m, 8u); // m <<= 8
+		__m64 k = color_unpacked;
+		k = _m_psubw(k, dest_unpacked); // k -= d[j]
+		k = _m_pmullw(k, _mm_set1_pi16(TVPOpacityOnOpacityTable65[l])); // k *= TVPOpacityOnOpacityTable65[l]
+		k = _m_paddw(k, m); // k += m
+		k = _m_psrlwi(k, 8u); // k >>= 8
+		k = _m_packuswb(k, _mm_setzero_si64());
+		dest[i] = (TVPNegativeMulTable65[l] << 24) | _mm_cvtsi64_si32(k);
+	}
+	_m_empty();
+#endif
+}
+
+TVP_GL_IA32_FUNC_DECL(void, TVPApplyColorMap65_a_c, (tjs_uint32 *dest, const tjs_uint8 *src, tjs_int len, tjs_uint32 color))
+{
+#if 1
+	FOREACH_CHANNEL_COLOR(
+		{
+			tjs_uint16 cc = c[j];
+			if (j % 4 == 3)
+			{
+				cc = 0x0100;
+			}
+			tjs_uint16 k = d[j];
+			k *= src[i];
+			k >>= 6u;
+			k = d[j] - k;
+			tjs_uint16 l = src[i];
+			l *= cc;
+			l >>= 6u;
+			l += k;
+			if (l & (1 << 15))
+			{
+				l = 0;
+			}
+			if (l & (~0xFF))
+			{
+				l = 0xFF;
+			}
+			d[j] = l;
+		}, len, color, dest);
+#else
+	__m64 color_unpacked = _m_por(_m_punpcklbw(_mm_cvtsi32_si64((color & 0xFFFFFF) | 0x01000000), _mm_setzero_si64()), _m_psllqi(_mm_cvtsi32_si64(0x100u), 0x30u));
+	for (tjs_int i = 0; i < len; i += 1)
+	{
+		__m64 src_m64 = _mm_set1_pi16(src[i]);
+		__m64 dest_unpacked = _m_punpcklbw(_mm_cvtsi32_si64(dest[i]), _mm_setzero_si64());
+		__m64 k = dest_unpacked;
+		k = _m_pmullw(k, src_m64); // k *= src[i]
+		k = _m_psrlwi(k, 6u); // k >>= 6
+		k = _m_psubw(dest_unpacked, k); // k = dest_unpacked - k
+		__m64 l = src_m64;
+		l = _m_pmullw(l, color_unpacked); // l *= c[j]
+		l = _m_psrlwi(l, 6u); // l >== 6
+		l = _m_paddw(l, k); // l += k
 		l = _m_packuswb(l, _mm_setzero_si64());
 		dest[i] = _mm_cvtsi64_si32(l);
 	}
--- visual/IA32_behavior_c/univtrans.c
+++ visual/IA32_behavior_c/univtrans.c
@@ -7,24 +7,10 @@
 //                                         //
 /////////////////////////////////////////////
 
-#ifdef _WIN32
-#ifdef __GNUC__
-#pragma GCC push_options
-#pragma GCC target("mmx")
-#define __MMX__
-#endif
-#ifdef __clang__
-#pragma clang attribute push (__attribute__((target("mmx"))), apply_to=function)
-#define __MMX__
-#endif
-#endif
-
-#include <mmintrin.h>
 #include "tvpgl_ia32_intf_behavior_c.h"
 
 TVP_GL_IA32_FUNC_DECL(void, TVPUnivTransBlend_c, (tjs_uint32 *dest, const tjs_uint32 *src1, const tjs_uint32 *src2, const tjs_uint8 *rule, const tjs_uint32 *table, tjs_int len))
 {
-#if 1
 	FOREACH_CHANNEL_2SRC(
 		{
 			tjs_uint32 k = s1[j];
@@ -44,25 +30,6 @@ TVP_GL_IA32_FUNC_DECL(void, TVPUnivTransBlend_c, (tjs_uint32 *dest, const tjs_ui
 			}
 			d[j] = k;
 		}, len, src1, src2, dest);
-#else
-	__m64            v12; // mm4
-	__m64            v13; // mm1
-
-	for (tjs_int i = 0; i < len; i += 1)
-	{
-		v12 = _mm_set1_pi16(*((const tjs_uint16 *)table + rule[i]));
-		v13 = _m_punpcklbw(_mm_cvtsi32_si64(src2[i]), _mm_setzero_si64());
-		__m64 k = _mm_cvtsi32_si64(src1[i]);
-		k = _m_punpcklbw(k, _mm_setzero_si64());
-		k = _m_psubw(k, v13);
-		k = _m_pmulhw(k, v12);
-		k = _m_psllwi(k, 1u);
-		k = _m_paddw(v13, k);
-		k = _m_packuswb(k, _mm_setzero_si64());
-		dest[i] = _mm_cvtsi64_si32(k);
-	}
-	_m_empty();
-#endif
 }
 
 TVP_GL_IA32_FUNC_DECL(void, TVPUnivTransBlend_switch_c, (tjs_uint32 *dest, const tjs_uint32 *src1, const tjs_uint32 *src2, const tjs_uint8 *rule, const tjs_uint32 *table, tjs_int len, tjs_int src1lv, tjs_int src2lv))
@@ -88,33 +55,7 @@ TVP_GL_IA32_FUNC_DECL(void, TVPUnivTransBlend_switch_c, (tjs_uint32 *dest, const
 			k += s2[j];
 			d[j] = k;
 		}, len, src1, src2, dest);
-#elif 0
-	__m64            v14; // mm4
-	__m64            v15; // mm1
-
-	for (tjs_int i = 0; i < len; i += 1)
-	{
-		if (rule[i] >= src1lv)
-		{
-			dest[i] = src1[i];
-			continue;
-		}
-		else if (rule[i] < src2lv)
-		{
-			dest[i] = src2[i];
-			continue;
-		}
-		v14 = _mm_set1_pi16(*((const tjs_uint16 *)table + rule[i]));
-		v15 = _m_punpcklbw(_mm_cvtsi32_si64(src2[i]), _mm_setzero_si64());
-		dest[i] = _mm_cvtsi64_si32(
-			_m_packuswb(
-				_m_paddw(v15, _m_psllwi(_m_pmulhw(_m_psubw(_m_punpcklbw(_mm_cvtsi32_si64(src1[i]), _mm_setzero_si64()), v15), v14), 1u)),
-				_mm_setzero_si64()));
-	}
-	_m_empty();
 #else
-	__m64            v14; // mm4
-	__m64            v15; // mm1
 	tjs_int ze = 1;
 	tjs_int ze2 = 0;
 
@@ -163,17 +104,28 @@ TVP_GL_IA32_FUNC_DECL(void, TVPUnivTransBlend_switch_c, (tjs_uint32 *dest, const
 		{
 			ze = 1;
 		}
-		v14 = _mm_set1_pi16(*((const tjs_uint16 *)table + rule[i]));
-		v15 = _m_punpcklbw(_mm_cvtsi32_si64(src2[i]), _mm_setzero_si64());
-		__m64 k = _mm_cvtsi32_si64(src1[i]);
-		k = _m_punpcklbw(k, _mm_setzero_si64());
-		k = _m_psubw(k, v15);
-		k = _m_pmulhw(k, v14);
-		k = _m_psllwi(k, 1u);
-		k = _m_paddw(v15, k);
-		k = _m_packuswb(k, _mm_setzero_si64());
-		dest[i] = _mm_cvtsi64_si32(k);
+		const tjs_uint8 *s1 = (const tjs_uint8 *)&src1[i];
+		const tjs_uint8 *s2 = (const tjs_uint8 *)&src2[i];
+		tjs_uint8 *d = (tjs_uint8 *)&dest[i];
+		for (tjs_int j = 0; j < 4; j += 1)
+		{
+			tjs_uint32 k = s1[j];
+			k -= s2[j];
+			k *= *((const tjs_uint16 *)table + rule[i]);
+			k >>= 16;
+			k <<= 1;
+			k += s2[j];
+			k &= 0xFFFF;
+			if (k & (1 << 15))
+			{
+				k = 0;
+			}
+			if (k & (~0xFF))
+			{
+				k = 0xFF;
+			}
+			d[j] = k;
+		}
 	}
-	_m_empty();
 #endif
 }
